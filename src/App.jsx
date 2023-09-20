1f02aa89052e31d451a8d29b16dda1cfae840255
Daniel-max640 <dan.msaj@gmail.com>
2023-09-20 00:15:23 -0600

    listo9

--- src/App.jsx
+++ src/App.jsx
@@ -46,15 +46,14 @@ function App() {
   console.log(weather)
 
   return (  
-    <div className='fondo2'>
+    
       <div>        
         <WeatherCard
           weather = {weather}
           temp={temp}       
         />
       </div>
-
-     </div>
+     
   )
 }
 
